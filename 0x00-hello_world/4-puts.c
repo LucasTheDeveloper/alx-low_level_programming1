244b56f8f0c1ad63d7e3ff477a3a1e1d215ecbbc
LucasTheDeveloper <koketsolucas092@gmail.com>
2023-06-15 05:05:23 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - we are using puts function to print a line instead of a printf
+ * Return:0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
