c3790c47c556e75ca48aa307990035b4eb388f01
LucasTheDeveloper <koketsolucas092@gmail.com>
2023-06-29 02:43:36 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/*
+* This program demonstrates pointer manipulation by assigning a value
+* to an integer through a pointer, resulting in a specific output.
+*/
+
 int main(void)
 {
 int n;
